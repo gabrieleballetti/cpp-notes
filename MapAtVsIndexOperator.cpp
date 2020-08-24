c21ec66cf7780c2ad51557c4cfddba50bb782cf8
Gabriele Balletti <gabriballets@gmail.com>
2020-08-24 23:15:30 +0200

    Ädd MapAtVsIndexOperator

---
+++ MapAtVsIndexOperator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+int main ()
+{
+	std::map<std::string,int> mymap = {
+				{ "alpha", 0 },
+				{ "beta", 0 },
+				{ "gamma", 0 } };
+
+	mymap.at("alpha") = 30;
+	mymap.at("beta") = 20;
+	mymap.at("gamma") = 10;
+
+	// this returns error
+	//std::cout << mymap.at("delta") << std::endl;
+
+	// this inserts a new element with that key and returns a reference to its mapped value (uses default constructor)
+	std::cout << mymap["delta"] << std::endl;
+
+	for (auto& x: mymap) {
+		std::cout << x.first << ": " << x.second << std::endl;
+	}
+
+	return 0;
+}
